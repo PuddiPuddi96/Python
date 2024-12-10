f23fd7fefc892da1c75878d251d6ac9b3177b29f
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-12-10 17:55:24 +0100

    [DAY-30]: apply handling exception

--- 100-days-of-code/day-26/NATO-alphabet/main.py
+++ 100-days-of-code/day-26/NATO-alphabet/main.py
@@ -3,6 +3,14 @@ import pandas
 alphabet_data_frame = pandas.read_csv("./data/nato_phonetic_alphabet.csv")
 alphabet_dictionary = {row.letter:row.code for (_, row) in alphabet_data_frame.iterrows()}
 
-word_from_user = input("Enter a word: ").upper()
-result = [alphabet_dictionary[letter] for letter in word_from_user]
-print(result)
+def generate_phonetic():
+    word_from_user = input("Enter a word: ").upper()
+    try:
+        result = [alphabet_dictionary[letter] for letter in word_from_user]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(result)
+
+generate_phonetic()
