024cffb57b602e20f70f5c6c2c43c2dbe2f19caf
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2025-02-16 20:30:12 +0100

    [FEAT]: Change info message

--- hangman-aulico-bot/default_messages.py
+++ hangman-aulico-bot/default_messages.py
@@ -1,6 +1,6 @@
 START_COMMAND_MESSAGE = 'You selected the start command'
 HELP_COMMAND_MESSAGE = 'Non meriti nessun aiuto, indovina la parola da solo. DEBOLE'
 CUSTOM_COMMAND_MESSAGE = 'You selected the custom command'
-INFO_COMMAND_MESSAGE = 'You selected the info command'
+INFO_COMMAND_MESSAGE = "Classico gioco dell'impiccato ma con le parole del linguaggio aulico"
 
 BOT_USERNAME = "@hangman_aulico_bot"
