3751bdcaa3c6db970bc75c979a8d4edcc7fda358
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2025-02-17 23:49:29 +0100

    [FEAT[: Change help message

--- hangman-aulico-bot/default_messages.py
+++ hangman-aulico-bot/default_messages.py
@@ -1,5 +1,5 @@
 START_COMMAND_MESSAGE = 'You selected the start command'
-HELP_COMMAND_MESSAGE = 'You selected the help command'
+HELP_COMMAND_MESSAGE = 'Non meriti nessun aiuto, indovina la parola da solo. DEBOLE'
 CUSTOM_COMMAND_MESSAGE = 'You selected the custom command'
 INFO_COMMAND_MESSAGE = 'You selected the info command'
 
